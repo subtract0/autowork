3f3d870964d5f2975f2dd2e3bc90bc6739adb5d9
zerofunnel <zerofunnel@tutamail.com>
2026-03-15 13:07:01 +0100

    Iteration 2: improve score to 22827.355118311578

--- target.py
+++ target.py
@@ -1,12 +1,8 @@
+from collections import defaultdict
+
 def process_transactions(users, transactions):
-    # Build a dictionary of user_id -> total amount (O(M) instead of O(N*M))
-    user_totals = {}
+    user_totals = defaultdict(float)
     for t in transactions:
-        uid = t['user_id']
-        user_totals[uid] = user_totals.get(uid, 0) + t['amount']
+        user_totals[t['user_id']] += t['amount']
     
-    # Now iterate through users and get the total (O(N))
-    result = []
-    for u in users:
-        result.append({'name': u['name'], 'total': user_totals.get(u['id'], 0)})
-    return result
+    return [{'name': u['name'], 'total': user_totals.get(u['id'], 0)} for u in users]
