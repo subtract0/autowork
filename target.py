4e6cca3c375c87430d22c4e34bbcfb1efd7393eb
zerofunnel <zerofunnel@tutamail.com>
2026-03-15 14:01:38 +0100

    Iteration 59: improve score to 35568.72066417219

--- target.py
+++ target.py
@@ -2,9 +2,10 @@ def process_transactions(users, transactions):
     n_users = len(users)
     user_totals = [0.0] * n_users
     
-    # Process all transactions with direct indexing
-    for t in transactions:
-        user_totals[t['user_id']] += t['amount']
+    # Cache attribute and method lookups
+    ut = user_totals  # local reference
     
-    # Pre-build result list more efficiently - use i directly since users[i]['id'] == i
+    for t in transactions:
+        ut[t['user_id']] += t['amount']
+    
     return [{'name': users[i]['name'], 'total': user_totals[i]} for i in range(n_users)]
