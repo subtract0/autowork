ce1a942c9f1e0cf27d5fbbe6e47f5b837de61fe9
zerofunnel <zerofunnel@tutamail.com>
2026-03-15 13:07:25 +0100

    Iteration 3: improve score to 33782.35723032607

--- target.py
+++ target.py
@@ -1,8 +1,11 @@
-from collections import defaultdict
-
 def process_transactions(users, transactions):
-    user_totals = defaultdict(float)
+    # Find max user id to size our array
+    max_user_id = max(u['id'] for u in users)
+    
+    # Use a list instead of dict for O(1) index access
+    user_totals = [0.0] * (max_user_id + 1)
+    
     for t in transactions:
         user_totals[t['user_id']] += t['amount']
     
-    return [{'name': u['name'], 'total': user_totals.get(u['id'], 0)} for u in users]
+    return [{'name': u['name'], 'total': user_totals[u['id']]} for u in users]
