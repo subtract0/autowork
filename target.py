d2318efbc5f7747ac0f91997472ca408fa83e3f2
zerofunnel <zerofunnel@tutamail.com>
2026-03-15 13:09:50 +0100

    Iteration 9: improve score to 36217.12164980949

--- target.py
+++ target.py
@@ -1,9 +1,14 @@
 def process_transactions(users, transactions):
-    # Pre-size array based on number of users (ids are 0 to len-1)
+    # Pre-size array based on max user_id seen in transactions
     n_users = len(users)
     user_totals = [0.0] * n_users
     
+    # Local variable caching for speed
+    totals = user_totals
+    add = totals.__setitem__
+    
     for t in transactions:
-        user_totals[t['user_id']] += t['amount']
+        uid = t['user_id']
+        totals[uid] = totals[uid] + t['amount']
     
     return [{'name': u['name'], 'total': user_totals[u['id']]} for u in users]
